e5ad17443d3d6d243be7f377be540e1bde5cfb1a
hillcarolu94877 <hill_carolu94877@gmx.com>
2025-03-05 18:07:25 +0000

    Add 2.java

---
+++ src/2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class MathsHomework {
+  public static void main(String[] args) {
+    // TODO: Your math homework solution here
+    System.out.println("Hello, world!");
+  }
+}
